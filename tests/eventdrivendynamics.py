6e87df08e9344ebec95865b73393c2419054a6b3
q <none>
2008-06-13 20:05:11 +0300

    changes to the eventdrivendynamics tests
    darcs-hash:20080613170511-22ea1-c7e7a705c571e56207a59922eb18da87832cb366.gz

--- tests/eventdrivendynamics.py
+++ tests/eventdrivendynamics.py
@@ -16,7 +16,7 @@ S.addParticle(pos=9, type_=3)
 #S.setCycleMoves()
 
 print "E:", S.energy()
-S.eddInit()
+S.eddEnable()
 S.lattsite.shape = S.lattShape
 print S.MLL
 print S.MLLr
@@ -31,3 +31,20 @@ for i in range(10):
     #print S.lattsite
     S.eddConsistencyCheck()
     print i, S.mctime, S.naccept, numpy.sum(S.lattsite != -1 ), S.energy(), S.hash()
+
+
+S1 = Grid2d()
+S1.makegrid(25, 25)
+S1.addParticleRandomDensity(.7, type_=3)
+S1.setCycleMoves()
+
+S2 = Grid2d()
+S2.makegrid(25, 25)
+S2.addParticleRandomDensity(.7, type_=3)
+S2.setCycleMoves()
+S2.eddEnable()
+
+for i in xrange(100):
+    S1.cycle(100)
+    S2.cycle(100)
+    print i, S1.mctime, S1.naccept, S2.mctime, S2.naccept
