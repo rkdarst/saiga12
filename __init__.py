f5ce1e81051ed1807ae53d3ce5bbc1cee926f017
rkd <rkd@zgib.net>
2008-09-27 21:38:31 +0300

    change to absolute imports
    - I was recently prompted to try to work out absolute and relative imports
      (prompted by trying to use -m on main).  What I really need here is
      relative imports everywhere, however, the problems are: PEP 328 is only
      in python2.5 (I want backwards compatibility to more than just that),
      and handling __main__ via -m doesn't work well yet (PEP 366 not
      implemented yet).
    - Thus, for now, I have changed all imports to absolute.  I will later
      work on a permenent solution once it becomes an issue again
    - For now, renaming the saiga12 module remains difficult.
    
    darcs-hash:20080927183831-51e1e-cfbe5c656f445549da40c8e1d9a578844b09dfa8.gz

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 
 __version__ = "dev"
-from main import *
 
-from geom.grid import Grid1d, Grid2d, Grid3d, GridHex2d, Grid3dHCP
+from saiga12.main import *
+from saiga12.geom.grid import Grid1d, Grid2d, Grid3d, GridHex2d, Grid3dHCP
