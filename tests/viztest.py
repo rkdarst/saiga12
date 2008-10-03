c33f5344df3f04d8252befb8c8099f85866eafcc
rkd <rkd@zgib.net>
2008-10-04 02:34:04 +0300

    big updates to visualization
    - fix one bug (grid_coords instead of coords)
    - add documentation (none existed before)
    - make it work in grand canonical (or any time particles are being
      added/removed)
    - added ways to configure particle radii and colors on a per-type basis.
    - updated viztest.py
    
    darcs-hash:20081003233404-51e1e-509d5a02d9ddcdb159168fac7af129228537e468.gz

--- tests/viztest.py
+++ tests/viztest.py
@@ -1,14 +1,13 @@
+# Richard Darst, 2008
 
-
-# 2D
-
+#Generic tests:
+import time
 import saiga12
-from saiga12.geom.grid import Grid2d
+from saiga12.geom.grid import Grid3d
 import saiga12.viz
 
-
-S = Grid2d()
-S.makegrid(25, 25)
+S = Grid3d()
+S.makegrid(10, 10, 10)
 S.addParticleRandomDensity(.05, 0)
 S.addParticleRandomDensity(.10, 1)
 S.addParticleRandomDensity(.15, 2)
@@ -17,8 +16,46 @@ S.addParticleRandomDensity(.25, 4)
 S.addParticleRandomDensity(.30, 5)
 S.addParticleRandomDensity(.35, 6)
 S.cycle(100000)
+
 V = saiga12.viz.VizSystem(S)
 
+V.vizColors[4] = saiga12.viz.visual.color.white
+V.vizRadius[4] = .1
+V.vizRadius[5] = .5
+# V.radius = .5  # set default radius
 
 V.vizMakeBox()
 V.vizDisplay()
+V.vizDisplay()
+
+S.addParticleRandomDensity(.40, 6)
+V.vizDisplay()
+
+# del V   # removes all objects
+time.sleep(2)
+
+
+
+
+#Try a grand canonical test:
+S = Grid3d()
+S.makegrid(10, 10, 10)
+S.setCycleMode('fredricksonandersen')
+S.beta = 1.
+S.addParticleRandomDensity(.10, 1)
+S.inserttype = 1
+S.eddEnable()
+
+V = saiga12.viz.VizSystem(S)
+V.vizMakeBox()
+V.vizDisplay()
+
+maxN = 0
+while True:
+    S.cycle(100)
+    maxN = max(maxN, S.N)
+    print S.mctime, maxN, S.N, len(V._display), S.beta
+    #time.sleep(.5)
+    V.vizDisplay()
+    if S.mctime > 2000: S.beta = 1/.1
+    if S.mctime > 5e6: S.beta = 1
--- viz.py
+++ viz.py
@@ -1,16 +1,70 @@
 # Richard Darst, April 2008
 
+"""Visualization utilities.
+
+This entire module requires python-visual, also known as vpython,
+debian package name 'python-visual', (import name `visual`).
+
+This module is primarily used via the `VizSystem` object.
+"""
+
 import saiga12
 import numpy
 import visual
 
 class VizSystem(object):
+    """Visualize the system using python-visual.
+
+    Here is how it works:
+      V = saiga12.viz.VizSystem(S)  # note, initialize with System object
+      V.vizMakeBox()                # from then on, you just use the V obj
+      V.vizDisplay()
+
+    then, to update atom locations, call display again:
+      V.vizDisplay()
+
+    There isn't yet an interface on System objects to display things--
+    maybe someday?
+
+    RADIUS:
+    To configure default particle radius, set
+       V.radius = radius
+    Default particle radious is .25
+    To set a radius for a specific type of particle, use
+       V.vizRadius[particleType] = radius
+
+    COLOR:
+    To configure particle colors, use:
+       V.vizColors[particleType] = color
+    To see what colors are available, do `pydoc visual.color` for
+    pre-defined colors and visual docs.
+    Default colors are 0:red, 1:white, 2:cyan, 3:green,
+                       4:darkblue, 5:darkgreen, 6:blue, otherwise white.
+
+    """
     def __init__(self, S):
         self.S = S
+        self._display = [ ]
         self._otherObjects = [ ]
+        self.vizColors = {
+            0: visual.color.red,    # (1,0,0)
+            1: visual.color.white,
+            2: visual.color.cyan,
+            3: visual.color.green,  # (0,1,0)
+            4: (0.,  .5, 0. ),
+            5: (0., 0. ,  .5),
+            6: visual.color.blue,   # (0,0,1)
+            }
+        self.radius = .25
+        self.vizRadius = { }
 
     def vizMakeBox(self):
-        """Put the simulation box on the visual display"""
+        """Put the simulation box on the visual display.
+
+        This method should only be called once (but won't die if you
+        call it more than once, but it might leak memory long-term).
+        Visual keeps references to objects around...
+        """
         if visual is None:
             print "visual module was not imported, visual siletly deactivated."
             return
@@ -38,39 +92,46 @@ class VizSystem(object):
         visual.cylinder(pos=(0,y,0), axis=(0, 0, z), radius=radius, color=c),
         visual.cylinder(pos=(x,y,0), axis=(0, 0, z), radius=radius, color=c),
         ))
-    vizColors = {
-        0: visual.color.red,    # (1,0,0)
-        1: visual.color.white,
-        2: visual.color.cyan,
-        3: visual.color.green,  # (0,1,0)
-        4: (0.,  .5, 0. ),
-        5: (0., 0. ,  .5),
-        6: visual.color.blue,   # (0,0,1)
-        }
 
     def vizDisplay(self):
+        """Display the position of atoms in the system.
+
+        Call this the first time to display the atoms on the visual.
+        Call it again to update all the positions.  You don't need to
+        re-make the box at every step, it behaves smartly and just
+        moves the particles around.
+        """
         if visual is None:
             return
-        c = self.vizColors
-        if not hasattr(self, "_display"):
-            display = [ ]
-            #print self.S.N
-            #print self.S.lattShape
-            for i in range(self.S.N):
-                pos = self.S.atompos[i]
-                coords = self.S.coords(pos)
-                display.append(visual.sphere(pos=coords,
-                                             radius=.25, #r, not d
-                         color=c.get(self.S.atomtype[i], visual.color.white)))
-                display[-1].opacity = .2
-                self._display = display
-        else:
-            display = self._display
-            for i in range(self.S.N):
-                pos = self.S.atompos[i]
-                coords = self.S.grid_coords(pos)
-                display[i].pos = coords
+        vizColors = self.vizColors
+        vizRadius = self.vizRadius
+        display = self._display
+        S = self.S
+        # Now go add/update all atom positions, etc.
+        for i in range(self.S.N):
+            pos =    S.atompos[i]
+            coords = S.coords(pos)
+            type_ =  S.atomtype[i]
+            radius = vizRadius.get(type_, self.radius)
+            color = vizColors.get(type_, visual.color.white)
+            # create the particle if not existing yet:
+            if len(display) <= i:
+                display.append(visual.sphere(pos=coords, radius=radius))
+                display[i].opacity = .2
+            # update it if it's already there (yes, it re-sets pos...)
+            display[i].visible = 1
+            display[i].pos = coords
+            display[i].color = color
+            display[i].radius = radius
+        # hide all higher particle numbers:
+        for i in range(self.S.N, len(display)):
+            display[i].visible = 0
+
     def __del__(self):
+        """Remove all shapes from the display.
+
+        Deleting the object removes all shapes from the display.
+        """
         display = self._display
         for i in range(len(self._display)):
             display[i].visible = 0
