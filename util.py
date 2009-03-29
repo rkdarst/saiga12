5776c76bd089acac331af3466ea862906ad78632
rkd <rkd@zgib.net>
2009-03-29 07:02:19 +0300

    implement numerically stable averager
    - Numerically stable on-line averaging class.
    
    darcs-hash:20090329040219-51e1e-6da767f29f5485e5c1099772a7c95bf65a661558.gz

--- util.py
+++ util.py
@@ -46,6 +46,53 @@ def getNewFrameIndex(frame_index, nFrames):
             visual.scene.center = visual.scene.originalCenter
     return frame_index
 
+class Averager(object):
+    """Numerically Stable Averager
+
+    Calculate averages and standard deviations in a numerically stable way.
+
+    From the 'On-Line Algorithm' from
+    http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+    """
+    def __init__(self):
+        self.n       = 0
+        self._mean    = 0.   # mean
+        self._M2     = 0.   # variance accumulator
+        #self._var_n  = 0.
+        #self._var_n1 = 0.
+    def add(self, value):
+        """Add a new number to the dataset.
+        """
+        n = self.n + 1
+        delta = value - self._mean
+        mean = self._mean + delta/n
+        M2 = self._M2 + delta*(value - mean)
+        self.n = n
+        self._mean = mean
+        self._M2 = M2
+
+    @property
+    def mean(self):
+        """Mean"""
+        return self._mean
+    @property
+    def std(self):
+        """Population Variance"""
+        return math.sqrt(self._M2 / self.n)
+    @property
+    def stdsample(self):
+        """Sample Variance"""
+        return math.sqrt(self._M2 / (self.n-1))
+    @property
+    def var(self):
+        """Population Standard Deviation"""
+        return self._M2 / self.n
+    @property
+    def varsample(self):
+        """Sample Standard Deviation"""
+        return self._M2 / (self.n-1)
+    
+
 
 def diff(fname0, *fileNames):
     frame0 = io_open(fname0)
