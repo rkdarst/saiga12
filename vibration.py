fe009470006b188ac5d82b6a4e83ff07f7ab1766
rkd <rkd@zgib.net>
2009-05-21 04:42:16 +0300

    vibration updates - add orthogonal mode, etc
    - othogonal directions only mode
    - phases are set to zero by default now
    - increase ease of configuration of the things.
    
    darcs-hash:20090521014216-51e1e-7a2f9bbe59507cf29b74955c3a21e065817fbfb4.gz

--- vibration.py
+++ vibration.py
@@ -5,6 +5,9 @@ import numpy
 
 import saiga12
 
+#mode = 'orthogonal'
+mode = None
+
 class SystemVibrations(object):
     def vib_init(self, loadFrom=None):
         """Generation of the random vibration parameters.
@@ -13,20 +16,45 @@ class SystemVibrations(object):
         from this object.
         """
         self.vibEnabled = True
-        realFrequency = .0125461564465151
+        realFrequency = .0125
         angFrequency = 2 * math.pi * realFrequency
-        self.vib_scale = .35
+        amplitude = .35
+        #self.vib_scale = .35
 
         if loadFrom is None:
-            self.vib_phases = numpy.random.uniform(low=0, high=2*math.pi,
-                                                   size=3*self.lattSize)
-            self.vib_phases.shape = self.lattSize, 3
-            self.vib_freqs = numpy.random.normal(loc=0, scale=angFrequency,
-                                                 size=3*self.lattSize)
-            self.vib_freqs.shape = self.lattSize, 3
+            #phases = lambda length:
+            #    numpy.random.uniform(low=0, high=2*math.pi, size=length)
+            phases = lambda length: numpy.zeros(length)
+            freqs = lambda length: \
+                    numpy.random.normal(loc=0,scale=angFrequency, size=length)
+            ampls = lambda length: \
+                numpy.random.normal(loc=0, scale=amplitude, size=length)
+            if mode == "orthogonal":
+                direction = numpy.random.randint(3, size=self.lattSize)
+                sliceobj = numpy.arange(self.lattSize), direction
+
+                # it is important that we use sin instead of cosine to
+                # keep all non-significant ones zero.
+                self.vib_phases = numpy.zeros(shape=(self.lattSize, 3))
+                self.vib_freqs  = numpy.zeros(shape=(self.lattSize, 3))
+                self.vib_ampls  = numpy.zeros(shape=(self.lattSize, 3))
+                self.vib_phases[sliceobj] = phases(self.lattSize)
+                self.vib_freqs[sliceobj]  = freqs(self.lattSize)
+                self.vib_ampls[sliceobj]  = ampls(self.lattSize)
+            else:
+                self.vib_phases = phases(3*self.lattSize)
+                self.vib_freqs  = freqs(3*self.lattSize)
+                self.vib_ampls  = ampls(3*self.lattSize)
+                self.vib_phases.shape = self.lattSize, 3
+                self.vib_freqs.shape  = self.lattSize, 3
+                self.vib_ampls.shape  = self.lattSize, 3
+
+            #from rkddp.interact import interact ; interact()
+
         else:
             self.vib_phases = loadFrom.vib_phases
             self.vib_freqs = loadFrom.vib_freqs
+            self.vib_ampls = loadFrom.vib_ampls
 
     def vib_offset(self, index=None):
         """Return an array of vibration offsets for coordinates.
@@ -43,7 +71,8 @@ class SystemVibrations(object):
             numpy.multiply(coordOffset, self.mctime, coordOffset)
             numpy.add(     coordOffset, self.vib_phases, coordOffset)
             numpy.sin(     coordOffset, coordOffset)
-            numpy.multiply(coordOffset, self.vib_scale , coordOffset)
+            #numpy.multiply(coordOffset, self.vib_scale, coordOffset)
+            numpy.multiply(coordOffset, self.vib_ampls, coordOffset)
             # update cache par:
             self.vib_cache[0] = self.mctime
         else:
