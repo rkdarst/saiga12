6c03aabb2ee2043966163db7b6e5a9ab588a3b9c
rkd <rkd@zgib.net>
2009-06-02 01:22:12 +0300

    fix ctcc dynamics coordinate wrappnig problem
    darcs-hash:20090601222212-51e1e-390f1c020568d61dea92b0dd8b5a1d36df099585.gz

--- ctccdynamics.py
+++ ctccdynamics.py
@@ -5,12 +5,41 @@ import numpy
 class CTCCDynamics(object):
 
     def ctcc_adjustCoords(self, coords, index=None):
+        """Adjust coords based on CTCC dynamics orientation.
+        """
+        #activeParticles = self.orient != -1
+        #firstCoords = coords[activeParticles]
+        #neighCoords = \
+        #   coords[self.conn[activeParticles, self.orient[activeParticles]]]
+        #newCoords = (3*firstCoords + neighCoords) / 4.
+        #coords0 = coords.copy()
+        #coords0[activeParticles] = newCoords
+        #return coords0
+
+
         activeParticles = self.orient != -1
         firstCoords = coords[activeParticles]
         neighCoords = \
            coords[self.conn[activeParticles, self.orient[activeParticles]]]
-        newCoords = (3*firstCoords + neighCoords) / 4.
-        return newCoords
+        #import fitz.interactnow
+        deltas = neighCoords - firstCoords
+
+        physicalShape = numpy.asarray(self.physicalShape)
+        deltas += .5 * physicalShape
+        numpy.mod(deltas, physicalShape, deltas)
+        deltas -= .5 * physicalShape
+
+        deltas /= 4.
+        coords = coords.copy() # we *need* to make a copy here...
+        coords[activeParticles] += deltas
+
+        #coords[coords==14.25] = 10.5
+        #coords[coords==-.25] = 3.5
+
+        #if not numpy.all(coords == coords0):
+        #    import fitz.interactnow
 
         #from fitz.interact import interact ; interact()
+        return coords
+
 
